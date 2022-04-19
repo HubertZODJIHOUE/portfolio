8ddc58f94d7b10b2bfa7f3a1fb06086ea42cb417
l3miage-zodjihok <hamegan252@gmail.com>
2022-04-19 16:57:02 +0200

    partie responsive et css (presque complet)

--- js/main.js
+++ js/main.js
@@ -38,20 +38,20 @@ $(document).ready(function () {
 
   // sticky navigation menu
 
-  let nav_offset_top = $(".header_area").height() + 50;
+  //   let nav_offset_top = $(".header_area").height() + 50;
 
-  function navbarFixed() {
-    if ($(".header_area").length) {
-      $(window).scroll(function () {
-        let scroll = $(window).scrollTop();
-        if (scroll >= nav_offset_top) {
-          $(".header_area .main-menu").addClass("navbar_fixed");
-        } else {
-          $(".header_area .main-menu").removeClass("navbar_fixed");
-        }
-      });
-    }
-  }
+  //   function navbarFixed() {
+  //     if ($(".header_area").length) {
+  //       $(window).trigger(function () {
+  //         let scroll = $(window).scrollTop();
+  //         if (scroll >= nav_offset_top) {
+  //           $(".header_area .main-menu").addClass("navbar_fixed");
+  //         } else {
+  //           $(".header_area .main-menu").removeClass("navbar_fixed");
+  //         }
+  //       });
+  //     }
+  //   }
 
-  navbarFixed();
+  //   navbarFixed();
 });
